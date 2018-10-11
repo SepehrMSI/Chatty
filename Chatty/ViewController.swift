cab343e800d224fc23e2280eef21d0bc9393a20c
Sepehr <sepehr.masoudi@gmail.com>
2018-10-11 16:11:17 -0700

    Initial Commit

---
+++ Chatty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatty
+//
+//  Created by Sepehr's Cool Macbook on 10/11/18.
+//  Copyright © 2018 Sepehr's Cool Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
